023c900c46eb887cdc2092cde2f0a5108ac5a5df
pai <hachikopolarcito321@gmail.com>
2022-02-26 08:35:57 -0500

    render the images

--- main.go
+++ main.go
@@ -15,13 +15,11 @@ import (
 )
 
 func upload(c echo.Context) error {
-	// read the passowrd.txt file
 	pass, err := os.Open("password.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer pass.Close()
-	// read the file
 	b, err := ioutil.ReadAll(pass)
 	if err != nil {
 		log.Fatal(err)
@@ -30,7 +28,6 @@ func upload(c echo.Context) error {
 	if password != string(b) {
 		return echo.ErrUnauthorized
 	}
-
 	file, err := c.FormFile("file")
 	if err != nil {
 		return err
@@ -38,7 +35,6 @@ func upload(c echo.Context) error {
 	if file.Size > 100485760 || file.Header.Get("Content-Type") != "image/jpeg" && file.Header.Get("Content-Type") != "image/png" && file.Header.Get("Content-Type") != "video/mp4" {
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid file type or size (max 100MB)")
 	}
-
 	src, err := file.Open()
 	if err != nil {
 		return err
@@ -50,8 +46,6 @@ func upload(c echo.Context) error {
 		return err
 	}
 	defer dst.Close()
-
-	// Copy
 	if _, err = io.Copy(dst, src); err != nil {
 		return err
 	}
@@ -68,22 +62,18 @@ func displayFiles(c echo.Context) error {
 	}
 
 	for _, file := range files {
-		json.NewEncoder(c.Response()).Encode(c.Scheme() + "://" + c.Request().Host + "/" + file.Name())
-
+		c.HTML(http.StatusOK, "<img src='"+c.Scheme()+"://"+c.Request().Host+"/"+file.Name()+"'>"+"</img>")
 	}
 	return nil
 }
 
 func main() {
 	e := echo.New()
-
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-
 	e.Static("/", "public")
 	e.POST("/upload", upload)
 	e.GET("/f", displayFiles)
-
 	port, ok := os.LookupEnv("PORT")
 
 	if ok == false {
